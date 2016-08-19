65be23ef1fb263da318b8f38a25b3c2de1aa8c0e
grassnest <xiaobin517@gmail.com>
2016-08-19 12:27:10 +0800

    Update books.py

--- python/books.py
+++ python/books.py
@@ -7,6 +7,7 @@ import os.path
 import sys
 import urllib
 import unicodedata
+import pyprind
 
 address = 'http://www.lidown.com/'
 code = {u'\u2022':u'·', u'\xbd':u'1/2', u'\u30fb':u'·', u'\xf6':u'o', u'\xf1':u'n', u'\u0469':u'', u'\u02a8':u'雪狮'}
@@ -17,6 +18,7 @@ def download(url, target):
 	u = urllib2.urlopen(url)
 	meta = u.info()
 	file_size = int(meta.getheaders('Content-Length')[0])
+	download_bar = pyprind.ProgBar(file_size, bar_char='#')
 	
 	f = open(target, 'wb')
 	
@@ -29,8 +31,8 @@ def download(url, target):
 		
 		f.write(buffer)
 		downloaded_bytes += block_size
-		print downloaded_bytes
-	
+		progress = downloaded_bytes*1.0/file_size * 100
+		download_bar.update(block_size)
 	f.close()
 
 def analysis(path, url):
